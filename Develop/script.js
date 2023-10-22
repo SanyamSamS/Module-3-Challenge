4d312c508c0709bc45a985e8a1cde03bc53d6d4b
SanyamSamS <sanyam.edu@gmail.com>
2023-10-22 17:58:22 -0400

    added an alert message if no characters are selected

--- Develop/script.js
+++ Develop/script.js
@@ -5,9 +5,19 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var passwordLength = prompt("What length should the password be?")
   var numbers = prompt("Include numbers?")
-  var lowCase = prompt("Include lowercase letters?")
+  var lowerCase = prompt("Include lowercase letters?")
   var upperCase = prompt("Include uppercase letters?")
   var specialCharacters = prompt("Include special characters?")
+
+  // Turn string into a number 
+  var lengthInt = parseInt(passwordLength)
+
+  // At least one character alert
+  if (!passwordLength && !numbers && !lowerCase && !upperCase && !specialCharacters) {
+    alert("At least one character type should be selected.")
+  }
+
+  return
 }
 
 
