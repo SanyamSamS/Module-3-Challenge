ae50801f840b3dc64b57acd5f34b858781360b03
SanyamSamS <sanyam.edu@gmail.com>
2023-10-22 16:42:54 -0400

    Added a prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Password generation function
+function generatePassword() {
+  var passwordLength = prompt("What length should the password be?")
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
